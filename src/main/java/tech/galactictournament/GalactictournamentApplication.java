9af5c408c8759c9de83ace7d6924ceafc8745af7
Hector Noguera <hector.noguera03@gmail.com>
2026-05-29 15:55:16 -0400

    Primer commit

---
+++ src/main/java/tech/galactictournament/GalactictournamentApplication.java
@@ -0,0 +1,13 @@
+package tech.galactictournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalactictournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalactictournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/galactictournament/GalactictournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.galactictournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalactictournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
