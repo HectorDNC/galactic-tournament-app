2e9ce7e9bb4185a3eed51b9b910311fe96798641
Hector Noguera <hector.noguera03@gmail.com>
2026-05-30 19:02:15 -0400

    Feat: Añadir configuración de Swagger y documentación para los endpoints de la API

---
+++ src/main/java/tech/galactictournament/config/OpenAPIConfig.java
@@ -0,0 +1,27 @@
+package tech.galactictournament.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Galactic Tournament API")
+                        .description("API para gestionar un torneo intergaláctico de combates entre especies")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Galactic Tournament")
+                                .url("https://galactic-tournament.local"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")));
+    }
+}
