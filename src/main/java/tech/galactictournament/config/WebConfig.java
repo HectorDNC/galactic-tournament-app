b6f16329eb65ab47f64e1c1c49c8b1b55ecb7812
Hector Noguera <hector.noguera03@gmail.com>
2026-05-31 08:22:38 -0400

    Fix: Actualizar versión de springdoc-openapi y añadir redirección a Swagger UI

--- src/main/java/tech/galactictournament/config/WebConfig.java
+++ src/main/java/tech/galactictournament/config/WebConfig.java
@@ -2,12 +2,18 @@ package tech.galactictournament.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
